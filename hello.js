3ea54e95b5a43543d0174c8895c41941c3b74afa
Safee1282 <safeesajid1282@gmail.com>
2024-12-04 12:49:48 +0530

    initial commit

---
+++ hello.js
@@ -0,0 +1,17 @@
+let student={name:"Areef",age:12};
+let ans = JSON.stringify(student)
+console.log(ans)
+let str = '{ "name": "Areef", "age": 12 }';
+let ob=JSON.parse(str);
+console.log(ob)
+
+function greet(name, callback){
+    console.log('Hi' + ''+name);
+    callback();
+}
+
+function callMe() {
+    console.log('I am callback function');
+}
+
+greet('Mujtaba', callMe)
